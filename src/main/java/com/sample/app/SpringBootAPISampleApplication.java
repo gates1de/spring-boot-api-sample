6faa92d115b0e8bbcaf413307388894cb04a93b5
yu-kadowaki <g031i043@gmail.com>
2018-03-19 23:38:19 +0900

    Rename project

--- src/main/java/com/sample/app/SpringBootAPISampleApplication.java
+++ src/main/java/com/sample/app/SpringBootAPISampleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSampleApplication {
+public class SpringBootAPISampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSampleApplication.class, args);
+		SpringApplication.run(SpringBootAPISampleApplication.class, args);
 	}
 }
